1d7edb1e9133f539cbab91bfec2c2d046ce77d4f
MahmudNiloy680 <mahmudniloy680work@gmail.com>
2025-06-23 21:13:52 +0600

    Add a new approach to Week_1-M_Cypher

--- Week_1/M_Cypher.cpp
+++ Week_1/M_Cypher.cpp
@@ -1,45 +1,83 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void up(int &x)
+// {
+//     x = (x == 9) ? 0 : x + 1;
+// }
+
+// void down(int &x)
+// {
+//     x = (x == 0) ? 9 : x - 1;
+// }
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+
+//     while (t--)
+//     {
+//         int n;
+//         cin >> n;
+
+//         int a[n];
+//         for (int i = 0; i < n; i++)
+//             cin >> a[i];
+        
+//         for (int i = 0; i < n; i++)
+//         {
+//             int k; string m;
+//             cin >> k >> m;
+
+//             for (int j = k - 1; j >= 0; j--)
+//             {
+//                 if (m[j] == 'D')
+//                     up(a[i]);
+//                 else 
+//                     down(a[i]);
+//             }
+//             cout << a[i] << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-
-void up(int &x)
-{
-    x = (x == 9) ? 0 : x + 1;
-}
-
-void down(int &x)
-{
-    x = (x == 0) ? 9 : x - 1;
-}
+#define ll long long
+#define nl '\n'
+#define tt(x) ll x; cin >> x; while (x--)
 
 int main()
 {
-    int t;
-    cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    while (t--)
+    tt(t)
     {
-        int n;
-        cin >> n;
+        int n; cin >> n;
+        vector<int> final(n);
+        for (int &x : final) cin >> x;
 
-        int a[n];
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
-        
         for (int i = 0; i < n; i++)
         {
-            int k; string m;
-            cin >> k >> m;
-
-            for (int j = k - 1; j >= 0; j--)
+            int mod = 0;
+            int x; string s;
+            cin >> x >> s;
+            for (auto i : s)
             {
-                if (m[j] == 'D')
-                    up(a[i]);
-                else 
-                    down(a[i]);
+                if (i == 'D') mod--;
+                else mod++;
+                mod %= 10;
             }
-            cout << a[i] << " ";
+
+            ((final[i] -= mod) %= 10) < 0 ? final[i]+= 10 : 0;
+            cout << final[i] << " ";
         }
-        cout << endl;
+        cout << nl;
     }
+
     return 0;
 }
