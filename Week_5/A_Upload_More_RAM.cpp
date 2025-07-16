cc7801069b785a0b28d2c91fe82ceb07b1354ce0
MahmudNiloy680 <mahmudniloy680work@gmail.com>
2025-07-16 18:58:46 +0600

    Complete Week 5 with Problem Link

--- Week_5/A_Upload_More_RAM.cpp
+++ Week_5/A_Upload_More_RAM.cpp
@@ -1,3 +1,4 @@
+// Problem Link: https://codeforces.com/problemset/problem/1987/A
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
--- Week_5/C_Angry_Monk.cpp
+++ Week_5/C_Angry_Monk.cpp
@@ -1,3 +1,4 @@
+// Problem Link: https://codeforces.com/problemset/problem/1992/B
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
